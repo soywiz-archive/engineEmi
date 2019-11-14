ef1c48f2c42c1dad0bb4956fa5e7d414baa7d1c9
Nicolai Emig <nicolai.emig@amseegym.de>
2019-11-14 19:35:01 +0100

    test

--- src/Main.kt
+++ src/Main.kt
@@ -5,4 +5,5 @@ fun main(){
     val meinKreis = Kreis(posX = 100.0, posY = 100.0 , fillStyle = "blue", radius = 100.0, context = Engine.context)
     Engine.registerCanvasElement(meinKreis)
     Engine.main()
+    // test
 }
