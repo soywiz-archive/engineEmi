35ef5a0de3e93f5b604a79d9af732d4581677c82
Nicolai Emig <nicolai.emig@amseegym.de>
2019-11-14 19:38:01 +0100

    Anweisungen schüler main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,9 @@ import engineEmi.Kreis
 fun main(){
     val meinKreis = Kreis(posX = 100.0, posY = 100.0 , fillStyle = "blue", radius = 100.0, context = Engine.context)
     Engine.registerCanvasElement(meinKreis)
+
+    // Engine.main() muss stehen bleiben
+    // Eigener Code muss überhalb stehen
     Engine.main()
-    // test
+
 }
