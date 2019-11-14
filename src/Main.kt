c5042539a1408b1c8584faa3e681e227895d2893
Nicolai Emig <nicolai.emig@amseegym.de>
2019-11-14 08:52:08 +0100

    Auskommentiert

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,7 @@ import engineEmi.Engine
 import engineEmi.Kreis
 
 fun main(){
-    val meinKreis = Kreis(posX = 100.0, posY = 100.0 , fillStyle = "blue", radius = 100.0, context = Engine.context)
-    Engine.registerCanvasElement(meinKreis)
+    //val meinKreis = Kreis(posX = 100.0, posY = 100.0 , fillStyle = "blue", radius = 100.0, context = Engine.context)
+    //Engine.registerCanvasElement(meinKreis)
     Engine.main()
 }
